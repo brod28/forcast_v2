6f41c375c32d2f6eb41ea29865abc73e975d4e36
DimaBenivo <dima@benivo.com>
2018-01-21 20:35:49 +0000

    first commit

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from forcast import views as forcast_views   
 from authorization import views as authorization_views
+from django.contrib import admin
 
 # route for 2 end points 
 # for  bigger apps better delegate routing to its own urls.py
 urlpatterns = [
     url(r'^api/auth/login/$', authorization_views.Login.as_view()),
     url(r'^api/forcast/today/(?P<city>.+)$', forcast_views.ForcastDetails.as_view()),
+    url(r'^admin/', admin.site.urls),
 ]
  
